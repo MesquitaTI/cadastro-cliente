abb16388f5e55cf8b55fec5a4bfb4e8e2ebb6ce5
MesquitaTI <mesquitasolucoestecnologicas@gmail.com>
2025-08-06 13:00:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,31 +6,31 @@ document.getElementById("formCliente").addEventListener("submit", function (e) {
   const cpf = document.getElementById("cpf").value;
   const telefone = document.getElementById("telefone").value;
   const email = document.getElementById("email").value;
-  const equipamentos = document.getElementById("equipamentos").value;
+  const equipamento = document.getElementById("equipamentos").value;
   const dataNascimento = document.getElementById("dataNascimento").value;
   const descricao = document.getElementById("descricao").value;
 
-  const dados = {
+  const data = {
     nome,
     cpf,
     telefone,
     email,
-    equipamentos,
+    equipamentos: equipamento,
     dataNascimento,
     descricao
   };
 
   fetch("https://script.google.com/macros/s/AKfycbwBasaHqKBAJCWGMUYLJeRoZzc1sH2IYxfSK6C_nPZX6jqIHurxfbnH69tI-YOIyElwaA/exec", {
     method: "POST",
-    body: JSON.stringify(dados),
+    body: JSON.stringify(data),
     mode: "no-cors"
   })
-    .then(() => {
-      alert("Cadastro enviado com sucesso!");
-      document.getElementById("formCliente").reset();
-    })
-    .catch((error) => {
-      alert("Erro ao enviar cadastro. Verifique a conexão ou o link do Google Script.");
-      console.error("Erro:", error);
-    });
+  .then(() => {
+    alert("Cadastro enviado com sucesso para o Google Sheets!");
+    document.getElementById("formCliente").reset();
+  })
+  .catch((error) => {
+    alert("Erro ao enviar cadastro. Verifique a conexão ou o link do Google Script.");
+    console.error("Erro:", error);
+  });
 });
